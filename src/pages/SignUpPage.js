30643f5beeca889f514cfd86f4e654ea261ce676
breno.breves <brenocsb@dcc.ufrj.br>
2023-08-16 20:48:03 -0300

    Layout finalizado

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { styled } from "styled-components";
+import Logo from "../components/Logo";
+import StyledLink from "../style/StyledLink";
+import StyledLoginInput from "../style/StyledLoginInput";
+import StyledLoginButton from "../style/StyledLoginButton";
 export default function SigUpPage(){
+
     return (
         <SCPageContentBox>
-            <SCLogoBox></SCLogoBox>
-
-
-            <Link to={"/"}>Login</Link>
+            <Logo/>
+            <SCStyledForm>
+                <StyledLoginInput placeholder="e-mail"></StyledLoginInput>
+                <StyledLoginInput placeholder="password"></StyledLoginInput>
+                <StyledLoginInput placeholder="username"></StyledLoginInput>
+                <StyledLoginInput placeholder="picture url"></StyledLoginInput>
+                <StyledLoginButton>Sign Up</StyledLoginButton>
+                <StyledLink to={"/"}>Switch back to log in</StyledLink>
+            </SCStyledForm>
         </SCPageContentBox>
     )
 }
@@ -18,8 +27,12 @@ const SCPageContentBox = styled.div`
     height:100%;
 `;
 
-const SCLogoBox = styled.div`
-    width:62%;
-    height:100%;
-    background-color:#151515;
+const SCStyledForm = styled.form`
+    display:flex;
+    flex-direction:column;
+    width:38%;
+    margin-top:274px;
+    align-items: center;
+    gap:14px;
+
 `;
