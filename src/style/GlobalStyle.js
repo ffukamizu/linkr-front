d3d6b4cde1fd0a20f35a514f256c4038ebf34671
breno.breves <brenocsb@dcc.ufrj.br>
2023-08-16 19:27:00 -0300

    Divisão de pastas, browser router e problema da root resolvido

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        background-color:#333333;
+        height:100%;
+    }
+`
+
+export default GlobalStyle
