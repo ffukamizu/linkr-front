ccac7f96e2935fc4247eda195e5131144b4b229d
ffukamizu <f.fukamizu@live.com>
2023-08-21 19:27:09 -0300

    fix: dowwngrade node to 16

--- src/App.js
+++ src/App.js
@@ -3,23 +3,38 @@ import '../src/style/App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/SessionContext.js';
 import { HashtagPage } from './pages/HashtagPage.js';
-import SignInPage from "./pages/SignInPage";
-import SignUpPage from "./pages/SignUpPage";
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 import { TimelinePage } from './pages/TimelinePage.js';
 import { UserPage } from './pages/UserPage.js';
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/timeline" element={<TimelinePage />} />
-          <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
-          <Route path="/user/:id" element={<UserPage />} />
-        </Routes>
-      </BrowserRouter>
-      </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<SignInPage />}
+                    />
+                    <Route
+                        path="/sign-up"
+                        element={<SignUpPage />}
+                    />
+                    <Route
+                        path="/timeline"
+                        element={<TimelinePage />}
+                    />
+                    <Route
+                        path="/hashtag/:hashtag"
+                        element={<HashtagPage />}
+                    />
+                    <Route
+                        path="/user/:id"
+                        element={<UserPage />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
