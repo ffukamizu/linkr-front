89ecbc56984460d3c5f9330da875849a8b820ed7
ffukamizu <f.fukamizu@live.com>
2023-08-21 20:33:08 -0300

    fix: dotenv

--- src/services/apiPost.js
+++ src/services/apiPost.js
@@ -3,23 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const publishService = async (link, text, token) => {
   const config = { headers: { Authorization: `Bearer ${token}` }};
   
   const body = { link };
   if(text) body.text = text;
 
-  return await axios.post(`${process.env.REACT_APP_API_URL}/posts`, body, config);
+  return await axios.post(`${API_URL}/posts`, body, config);
 };
 
 export const deleteService = async (id, token) => {
   const config = { headers: { Authorization: `Bearer ${token}` }};
 
-  return axios.delete(`${process.env.REACT_APP_API_URL}/posts/${id}`, config);
+  return axios.delete(`${API_URL}/posts/${id}`, config);
 };
 
 export const editService = async (id, text, token) => {
   const config = { headers: { Authorization: `Bearer ${token}` }};
 
-  return axios.patch(`${process.env.REACT_APP_API_URL}/posts/${id}`, { text }, config);
+  return axios.patch(`${API_URL}/posts/${id}`, { text }, config);
 };
