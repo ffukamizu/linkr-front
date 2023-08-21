91816930235c6a984225fbb409b77af26e7c864b
ffukamizu <f.fukamizu@live.com>
2023-08-21 20:26:22 -0300

    fix: imported dotenv

--- src/services/apiPost.js
+++ src/services/apiPost.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const publishService = async (link, text, token) => {
   const config = { headers: { Authorization: `Bearer ${token}` }};
--- src/services/utils.js
+++ src/services/utils.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const server = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
